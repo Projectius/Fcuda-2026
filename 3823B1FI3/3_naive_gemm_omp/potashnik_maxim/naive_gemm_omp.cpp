97033f8340048c12a99c520334febf5d04d78011
Maxim <scourgetp@gmail.com>
2026-05-09 15:22:52 +0300

    [TASK3] Loop-unroll by hands

--- 3823B1FI3/3_naive_gemm_omp/potashnik_maxim/naive_gemm_omp.cpp
+++ 3823B1FI3/3_naive_gemm_omp/potashnik_maxim/naive_gemm_omp.cpp
@@ -9,7 +9,9 @@
 2. Better cache usage (IJK -> IKJ)
 3. Calculating a[i][k] out of j-loop
 4. SIMD in inner loop
-5. Using pointers to (MAYBE) simplify data access (and help SIMD)
+5. Using pointers to (MAYBE) simplify data access
+6. Unrolled loop 
+Did it by my hands because it help when I was testing in on my laptop
 */
 
 #include "naive_gemm_omp.h"
@@ -20,15 +22,18 @@ std::vector<float> NaiveGemmOMP(const std::vector<float>& a, const std::vector<f
 #pragma omp parallel for schedule(static)
     for (int i = 0; i < n; i++) {
         for (int k = 0; k < n; k++) {
-            float a_ik = a[i * n + k]; 
+            float a_ik = a[i * n + k];
             float* c_i = &c[i * n];
             const float* b_k = &b[k * n];
 
 #ifdef __GNUC__
 #pragma omp simd
 #endif
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j += 4) {
                 c_i[j] += a_ik * b_k[j];
+                c_i[j + 1] += a_ik * b_k[j + 1];
+                c_i[j + 2] += a_ik * b_k[j + 2];
+                c_i[j + 3] += a_ik * b_k[j + 3];
             }
         }
     }
